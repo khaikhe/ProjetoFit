3ba0198c467254ee4e1c768f1c3e510e0e35e7ca
khaikhe <kay.gc123@gmail.com>
2024-08-04 15:39:57 -0300

    ATT3.5

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,68 +1,48 @@
+
+import User from "@/models/user";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-import { PrismaClient } from "@prisma/client";
+import connect from "@/utils/db";
 
-const prisma = new PrismaClient();
 
-const authOptions = {
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      async authorize(credentials) {
-        try {
-          await prisma.$connect();
-          const user = await prisma.user.findUnique({
-            where: { email: credentials.email },
-          });
+const options = NextAuth({
+    providers: [CredentialsProvider({
+        id: "Credentials",
+        name: "Credentials",
+        async authorize(credentials){
+            await connect();
+            try {
+                const user = await User.findOne({
+                    email: credentials.email,
+                });
 
-          if (user) {
-            const validPassword = await bcrypt.compare(
-              credentials.password,
-              user.password
-            );
+                if (user) {
+                    const validPassword = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    );
 
-            if (validPassword) {
-              return user;
-            } else {
-              throw new Error("Credenciais erradas!");
-            }
-          } else {
-            throw new Error("Email errado!");
-          }
-        } catch (error) {
-          throw new Error(error.message);
-        } finally {
-          await prisma.$disconnect();
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login", // Página personalizada de login
-    error: "/login",  // Página personalizada de erro
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async session({ session, token, user }) {
-      session.user = user;
-      return session;
-    },
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-  },
-  debug: true,
-};
-
-export async function GET(request) {
-  return NextAuth(request, authOptions);
+                    if (validPassword) {
+                        return user;
+                    } else {
+                        throw new Error ("Credenciais erradas!");
+                    }
+                }
+                    else{
+                        throw new Error ("Email errado!");
+                    }
+                
+        
+    } catch(error){
+        throw new Error(error);
 }
+}})],
 
-export async function POST(request) {
-  return NextAuth(request, authOptions);
-}
+    pages: {
+        error:"/login",
+    },
+    secret:process.env.NEXTAUTH_SECRET
+});
+
+export {options as GET, options as POST };
