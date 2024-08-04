43b0bba8532b31f88d77110f09e3f6a77c3afd43
khaikhe <kay.gc123@gmail.com>
2024-08-04 15:28:08 -0300

    att 2.0

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,47 +1,65 @@
-
-import User from "@/models/user";
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-import connect from "@/utils/db";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
-const options = NextAuth({
-    providers: [CredentialsProvider({
-        id: "Credentials",
-        name: "Credentials",
-        async authorize(credentials){
-            await connect();
-            try {
-                const user = await User.findOne({
-                    email: credentials.email,
-                });
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        try {
+          const user = await prisma.user.findUnique({
+            where: { email: credentials.email },
+          });
 
-                if (user) {
-                    const validPassword = await bcrypt.compare(
-                        credentials.password,
-                        user.password
-                    );
+          if (user) {
+            const validPassword = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
 
-                    if (validPassword) {
-                        return user;
-                    } else {
-                        throw new Error ("Credenciais erradas!");
-                    }
-                }
-                    else{
-                        throw new Error ("Email errado!");
-                    }
-                
-        
-    } catch(error){
-        throw new Error(error);
-}
-}})],
-
-    pages: {
-        error:"/login",
+            if (validPassword) {
+              return user;
+            } else {
+              throw new Error("Credenciais erradas!");
+            }
+          } else {
+            throw new Error("Email errado!");
+          }
+        } catch (error) {
+          throw new Error(error.message);
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login", // Página personalizada de login
+    error: "/login",  // Página personalizada de erro
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user = user;
+      return session;
     },
-});
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
+  debug: true,
+};
 
-export {options as GET, options as POST };
+export async function GET(request) {
+  return NextAuth(request, authOptions);
+}
+
+export async function POST(request) {
+  return NextAuth(request, authOptions);
+}
