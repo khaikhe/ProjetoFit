8159f8db774508744fef777fb54b4798f4018fc7
khaikhe <kay.gc123@gmail.com>
2024-08-04 18:38:26 -0300

    5,5

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,48 +1,42 @@
-
-import User from "@/models/user";
+// /pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import connect from "@/utils/db";
-
+import User from "@/models/user";
 
 const options = NextAuth({
-    providers: [CredentialsProvider({
-        id: "Credentials",
-        name: "Credentials",
-        async authorize(credentials){
-            await connect();
-            try {
-                const user = await User.findOne({
-                    email: credentials.email,
-                });
-
-                if (user) {
-                    const validPassword = await bcrypt.compare(
-                        credentials.password,
-                        user.password
-                    );
-
-                    if (validPassword) {
-                        return user;
-                    } else {
-                        throw new Error ("Credenciais erradas!");
-                    }
-                }
-                    else{
-                        throw new Error ("Email errado!");
-                    }
-                
-        
-    } catch(error){
-        throw new Error(error);
-}
-}})],
-
-    pages: {
-        error:"/login",
-    },
-    secret:process.env.NEXTAUTH_SECRET
+  providers: [
+    CredentialsProvider({
+      id: "Credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          if (user) {
+            const validPassword = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (validPassword) {
+              return user;
+            } else {
+              throw new Error("Invalid credentials");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error.message);
+        }
+      }
+    })
+  ],
+  pages: {
+    error: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET
 });
 
-export {options as GET, options as POST };
+export default options;
