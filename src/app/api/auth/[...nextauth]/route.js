355f3eccc0d4ecb534ef49ca14be42622c3ab6c8
khaikhe <kay.gc123@gmail.com>
2024-08-12 13:07:31 -0300

    ATT 7.0

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,48 +1,42 @@
-
 import User from "@/models/user";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import connect from "@/utils/db";
 
-
 const options = NextAuth({
-    providers: [CredentialsProvider({
-        id: "Credentials",
-        name: "Credentials",
-        async authorize(credentials){
-            await connect();
-            try {
-                const user = await User.findOne({
-                    email: credentials.email,
-                });
+    providers: [
+        CredentialsProvider({
+            id: "Credentials",
+            name: "Credentials",
+            async authorize(credentials) {
+                await connect();
+                try {
+                    const user = await User.findOne({ email: credentials.email });
 
-                if (user) {
-                    const validPassword = await bcrypt.compare(
-                        credentials.password,
-                        user.password
-                    );
+                    if (user) {
+                        const validPassword = await bcryptjs.compare(credentials.password, user.password);
 
-                    if (validPassword) {
-                        return user;
+                        if (validPassword) {
+                            return user;
+                        } else {
+                            throw new Error("Credenciais erradas!");
+                        }
                     } else {
-                        throw new Error ("Credenciais erradas!");
+                        throw new Error("Email errado!");
                     }
+
+                } catch (error) {
+                    throw new Error(error.message);
                 }
-                    else{
-                        throw new Error ("Email errado!");
-                    }
-                
-        
-    } catch(error){
-        throw new Error(error);
-}
-}})],
+            }
+        })
+    ],
 
     pages: {
-        error:"/login",
+        error: "/login",
     },
-    secret:process.env.NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET
 });
 
-export {options as GET, options as POST };
+export { options as GET, options as POST };
